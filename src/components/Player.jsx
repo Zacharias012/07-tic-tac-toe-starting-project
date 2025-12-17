adaf3bd642ec72b327482d17881392a27435e5e9
zacharias <zachariaswagner@yahoo.de>
2025-12-17 19:48:08 +0100

    implement Two-Way-Binding for editing the player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,29 @@
 import {useState} from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing(prevState => !prevState);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     let buttonCaption = "Edit";
 
     if (isEditing) {
-        playerName = <input type="text" required value={name}/>;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
         buttonCaption = "Save";
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{buttonCaption}</button>
